c8165586ebbfb2f3024d48c5f7574517e7c67f94
Ayg0 <aygogyseven@gmail.com>
2023-05-06 13:43:27 +0100

    new

--- general.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GENERAL_HPP
-# define GENERAL_HPP
-# include <iostream>
-# include <unistd.h>
-
-
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
-#include "general.hpp"
+#include <iostream>
+#include <ncurses.h>
+#include <termios.h>
 
-int	main(int ac, char **av){
-	if (ac != 2)
-		return (write(2,"Error, Enter FileName\n", 22));
-	/*
-		open_file();
-		read_file();
-		get_terminal_prop();
-		clear_window();
-		show_it_all();
-		// Functionalities:
-			-> moving cursor;
-			-> insert and delete;
-			-> save;
-	*/
-	
+typedef	struct _pos{
+	int	x;
+	int	y;
+} pos;
+
+class _term{
+	int		flags;
+	pos		curser;
+	std::string	buff;
+
+	public:
+	int	init_term();
+	int	raw_mode();
+	int	edit_mode();
+	int	add();
+	int	rm();
+	int	mv_cursor();
+	int	
+};
+
+
+
+int	main(){
+	initscr();
+	cbreak();
+	noecho();
+	// keypad(stdscr, TRUE);
+	int h, w;
+	getmaxyx(stdscr, h, w);
+	wmove(stdscr, 0, 0);
+	waddch(stdscr, 'a');
+	wrefresh(stdscr);
+	endwin();
+	return (0);
 }
