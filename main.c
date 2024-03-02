4093634b6a839319393170fcca09021ebf7403b1
Ayg0 <taha.eddafili@gmail.com>
2024-03-02 19:27:13 +0100

    file reading (not finished yet)

--- main.c
+++ main.c
@@ -1,34 +1,52 @@
 #include "vinro.h"
 #include <ncurses.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 unsigned char _CLOSE_WINDOW = 0;
 vinroData	data;
 buffer		textBuffer;
+FILE		*file;
 
 
-void loadData(){
+void loadData(char *fileName){
+	char line[data.maxWidth];
+
+	file = fopen(fileName, "a+");
+	if (!file)
+		displayError("File \?\?!");
+	//appendRow("Hello World", 12, 0);
+	//appendRow("Hello guys!", 12, 1);
+	//appendRow("Hello guys!", 12, 2);
+	rewind(file);
+	int i = 0;
+	while (fgets(line, sizeof(line), file) != NULL) {
+        appendRow(line, strlen(line), i);
+		i++;
+    }
+}
+
+void initTextBuffer(){
 	textBuffer.nbRows = 0;
 	textBuffer.usedRows = 0;
 	textBuffer.cursorPos.x = 0;
 	textBuffer.cursorPos.y = 0;
 	textBuffer.lines = NULL;
-	appendRow("Hello World", 12, 0);
-	appendRow("Hello guys!", 12, 1);
-	appendRow("Hello guys!", 12, 2);
 }
 
 void initVinro(){
     initscr();
+	initTextBuffer();
     raw();
     keypad(stdscr, TRUE);
 	noecho();
-	loadData();
     getmaxyx(stdscr, data.maxHeight, data.maxWidth);
 }
 
 void destructVinro(){
     endwin();
+	fclose(file);
 }
 
 void displayError(char *err){
@@ -41,7 +59,7 @@ char outputBuffer(){
     uint32_t i = 0;
     while (i < data.maxHeight) {
 		if (i < textBuffer.usedRows)
-			printw("%s\n", textBuffer.lines[i].line);
+			printw("%s", textBuffer.lines[i].line);
 		else
 	        printw("~\n");
         i++;
@@ -56,8 +74,11 @@ char getInput(){
     return 0;
 }
 
-int main(){
+int main(int ac, char **av){
+	if (ac != 2)
+		displayError("didn't specify fileName");
     initVinro();
+	loadData(av[1]);
     while (!_CLOSE_WINDOW) {
         refresh();
         outputBuffer();
